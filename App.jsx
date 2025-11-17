b8f79271c0f3aa870963ddaa7425ab2508533f9f
Berkant Gunes <gunesberkant96@gmail.com>
2025-11-17 22:21:24 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import InvoicePage from "./pages/InvoicePage";
+import CustomerPage from "./pages/CustomerPage";
+import StatisticPage from "./pages/StatisticPage";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element= {<HomePage />} />
+        <Route path="/cart" element= {<CartPage />} />
+        <Route path="/invoices" element = {<InvoicePage />} />
+        <Route path="/customers" element = {<CustomerPage />} />
+        <Route path="/statistic" element = {<StatisticPage />} />
+        <Route path="/register" element = {<Register />} />
+        <Route path="/login" element = {<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
