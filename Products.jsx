f1534dd2efa944221894c2dff625106d64ff7fe9
Berkant Gunes <gunesberkant96@gmail.com>
2025-11-27 18:55:14 +0300

    Add files via upload

--- Products.jsx
+++ Products.jsx
@@ -1,91 +1,39 @@
+import { useEffect, useState } from "react"
 
 
 const Products = () => {
-  return (
-    <div className="products-wrapper grid grid-cols-card gap-4">
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const getProducts = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/products/get-all");
+                const data = await res.json();
+                setProducts(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getProducts();
+    }, []);
+
+    return (
+        <div className="products-wrapper grid grid-cols-card gap-4">
+            {products.map((item) => (
+                <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
+                    <div className="product-img">
+                        <img src={item.img} alt="" className="h-28 object-cover w-full border-b" />
+                    </div>
+                    <div className="product-info flex flex-col p-3">
+                        <span className="font-bold">{item.title}</span>
+                        <span>{item.price}$</span>
+                    </div>
+                </div>
+            ))}
+
         </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-        <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="product-img">
-                <img src="https://i0.wp.com/www.bikahvearasi.com/wp-content/uploads/2022/08/4-1.jpg?resize=750%2C500&ssl=1" alt="" className="h-28 object-cover w-full border-b" />
-            </div>
-            <div className="product-info flex flex-col p-3">
-                <span className="font-bold">Espresso </span>
-                <span>7 $</span>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Products
